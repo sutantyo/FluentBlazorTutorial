be7988c39d2aa849f0aaf227409b5957df6436c8
Daniel Sutantyo <daniel.sutantyo@vectorrisk.com>
2024-11-12 23:54:07 +1100

    Added drag and drop

---
+++ Components/Models/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicTutorial.Components.Models;
+
+public class Game(int gameId, string name, int rating) {
+  
+  public int GameId { get; set; } = gameId;
+  public string Name { get; set; } = name;
+  public int Rating { get; set; } = rating;
+
+}
